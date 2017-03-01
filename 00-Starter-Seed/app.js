88f48dd0c3b5a13b79a1eb8ec5465bfec65c7ce6
Anshul Dutta <AnshulKumar.Dutta@news.co.uk>
2017-03-01 16:15:33 +0000

    Starter-seed: Got it working up to the point of callback. Can't view the user info yet

--- 00-Starter-Seed/app.js
+++ 00-Starter-Seed/app.js
@@ -6,10 +6,37 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var dotenv = require('dotenv');
+var passport = require('passport');
+var Auth0Strategy = require('passport-auth0');
+var user = require('./routes/user');
 
 dotenv.load();
 
 var routes = require('./routes/index');
+var user = require('./routes/user');
+
+var strategy = new Auth0Strategy({
+    domain:       process.env.AUTH0_DOMAIN,
+    clientID:     process.env.AUTH0_CLIENT_ID,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET,
+    callbackURL:  process.env.AUTH0_CALLBACK_URL
+  }, function(accessToken, refreshToken, extraParams, profile, done) {
+    // accessToken is the token to call Auth0 API (not needed in the most cases)
+    // extraParams.id_token has the JSON Web Token
+    // profile has all the information from the user
+    return done(null, profile);
+  });
+
+passport.use(strategy);
+
+// This can be used to keep a smaller payload
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
 
 var app = express();
 
@@ -28,6 +55,8 @@ app.use(session({
   resave: true,
   saveUninitialized: true
 }));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
--- 00-Starter-Seed/routes/index.js
+++ 00-Starter-Seed/routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var passport = require('passport');
 var router = express.Router();
 
 var env = {
@@ -9,8 +10,28 @@ var env = {
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', env: env });
+  res.render('index', { title: 'Express', env: env, loginUrl: 'TODO- Sorry' });
 });
+
+
+// Render the login template
+router.get('/login',
+  function(req, res){
+    res.render('login', { env: process.env });
+  });
+
+// Perform session logout and redirect to homepage
+router.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+// Perform the final stage of authentication and redirect to '/user'
+router.get('/callback',
+  passport.authenticate('auth0', { failureRedirect: '/url-if-something-fails' }),
+  function(req, res) {
+    res.redirect(req.session.returnTo || '/user');
+  });
 
 
 module.exports = router;
